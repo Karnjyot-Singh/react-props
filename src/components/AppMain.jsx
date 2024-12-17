e3059a46fe66a44e26f157d92b06c3864b499081
Karnjyot <krnjyotsingh2004@email.com>
2024-12-18 00:03:32 +0100

    feat: milstone n2

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,25 +1,28 @@
 import AppCard from "./AppCard";
 import PostsArray from "../data/data";
 
-const printPosts = () => {
-    return PostsArray.map((curPost) => (
+const printPosts = (posts) => {
+    return posts.map((curPost) => (
         <div className="col" key={curPost.id}>
             <AppCard
                 image={curPost.image}
                 title={curPost.title}
                 tags={curPost.tags}
                 content={curPost.content}
+                published={curPost.published}
             />
         </div>
     ))
 }
 
 const AppMain = () => {
+    const publishedPosts = PostsArray.filter(post => post.published);
+
     return(
         <main>
             <section>
                 <div className="row">
-                    {printPosts()}
+                {publishedPosts.length > 0 ? printPosts(publishedPosts) : <p>Nessun Post disponibile</p>}
                 </div>
             </section>
         </main>
